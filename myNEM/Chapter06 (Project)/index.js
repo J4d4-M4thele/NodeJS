c6bbeee789d6a3902731f0d6a45079415a4a4780
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-07-19 14:50:14 +0200

    update

--- myNEM/Chapter06 (Project)/index.js
+++ myNEM/Chapter06 (Project)/index.js
@@ -48,6 +48,8 @@ app.post('/posts/store', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('App listening on port 3000')
+app.listen(4000, () => {
+    console.log('App listening on port 4000')
 })
+
+//pg 65
