d461ac4cda2de0adc24330a3669d129f24888213
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-07-21 15:35:45 +0200

    update

--- myNEM/Chapter14 (Project)/controllers/newPost.js
+++ myNEM/Chapter14 (Project)/controllers/newPost.js
@@ -1,7 +1,8 @@
 module.exports = (req, res) => {
-    if (req.session.userId) { //check if session has user ID
-        return res.render('create'); //if it does have user id then show create blog post page
-    } else {
-        res.redirect('/auth/login');
+    if (req.session.userId) {
+        return res.render("create", {
+            createPost: true
+        });
     }
-};
+    res.redirect('/auth/login')
+}
